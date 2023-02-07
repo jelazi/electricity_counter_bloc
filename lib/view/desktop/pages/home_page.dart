224af6a2451690fe323437bd7e62abfed609fdf6
jelazi <lzizka@gmail.com>
2023-02-07 17:25:20 +0100

    test commit

--- lib/view/desktop/pages/home_page.dart
+++ lib/view/desktop/pages/home_page.dart
@@ -1,70 +1,95 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:f_logs/f_logs.dart';
 import 'package:flutter/material.dart';
 
 import '../../../blogs/bloc_export.dart';
 import '../../../localization/app_localizations.dart';
+import '../../../models/entry.dart';
+import '../../../models/user.dart';
 
 class HomePageDesktop extends StatelessWidget {
   HomePageDesktop({Key? key}) : super(key: key);
-  ScrollController scrollController = ScrollController();
+  final ScrollController _horizontalScrollController = ScrollController();
+  final ScrollController _verticalScrollController = ScrollController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(
         child: BlocBuilder<UsersBloc, UsersState>(builder: (context, state) {
-      return Column(
-        children: [
-          Text(AppLocalizations.of(context).translate('usersList')),
-          Container(
+      final users = List.from(state.users);
+      var max = 0;
+      users.forEach((user) {
+        if (user.listEntries.length > max) {
+          max = user.listEntries.length;
+        }
+      });
+      return Column(children: [
+        Text(AppLocalizations.of(context).translate('usersList')),
+        Container(
             width: MediaQuery.of(context).size.width * 0.9,
             height: MediaQuery.of(context).size.height * 0.4,
-            child: Scrollbar(
-              controller: scrollController,
-              thumbVisibility: true,
-              thickness: 10,
-              child: ListView(
-                  controller: scrollController,
-                  scrollDirection: Axis.horizontal,
-                  children: state.users
-                      .map((user) => SizedBox(
-                            width: 400,
-                            child: Column(
-                              children: [
-                                Card(
-                                    color: Colors.red[50],
-                                    child: ListTile(
-                                      title: Text(user.name),
-                                      trailing: IconButton(
-                                        icon: const Icon(Icons.delete),
-                                        onPressed: () => context
-                                            .read<UsersBloc>()
-                                            .add(RemoveUser(user: user)),
-                                      ),
-                                    )),
-                                Expanded(
-                                  child: ListView(
-                                    children: user.listEntries
-                                        .map((entry) => Card(
-                                                child: ListTile(
-                                              leading:
-                                                  Text(entry.value.toString()),
-                                              trailing: IconButton(
-                                                icon: const Icon(Icons.delete),
-                                                onPressed: () => context
-                                                    .read<UsersBloc>()
-                                                    .add(RemoveEntry(
-                                                        entry: entry)),
-                                              ),
-                                            )))
-                                        .toList(),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ))
-                      .toList()),
-            ),
-          ),
-        ],
-      );
+            child: Table(
+              defaultColumnWidth: const FixedColumnWidth(200),
+              children: state.users.map((user) {
+                TableCell cellName = TableCell(child: NameUserCard(user: user));
+                var entries = <TableCell>[];
+                entries.add(cellName);
+                for (var i = 0; i < max; i++) {
+                  if (user.listEntries.length > i) {
+                    entries.add(TableCell(
+                        child: EntryCard(
+                      entry: user.listEntries[i],
+                    )));
+                  } else {
+                    entries.add(TableCell(child: Container()));
+                  }
+                }
+                return TableRow(children: entries);
+              }).toList(),
+            ))
+      ]);
     })));
+  }
+}
+
+class EntryCard extends StatelessWidget {
+  Entry entry;
+  EntryCard({
+    Key? key,
+    required this.entry,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Text(entry.value.toString()),
+      trailing: IconButton(
+        icon: const Icon(Icons.delete),
+        onPressed: () =>
+            context.read<UsersBloc>().add(RemoveEntry(entry: entry)),
+      ),
+    ));
+  }
+}
+
+class NameUserCard extends StatelessWidget {
+  User user;
+  NameUserCard({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        color: Colors.red[50],
+        child: ListTile(
+          title: Text(user.name),
+          trailing: IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () =>
+                context.read<UsersBloc>().add(RemoveUser(user: user)),
+          ),
+        ));
   }
 }
